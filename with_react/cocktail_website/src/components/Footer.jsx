66e1028db39a66d9b894639283a011098144e71b
Stancz Boglár Anna <sbanna0422@gmail.com>
2025-09-23 23:16:23 +0200

    feat(footer): added footer to App.jsx

--- with_react/cocktail_website/src/components/Footer.jsx
+++ with_react/cocktail_website/src/components/Footer.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
-function Footer() {
+export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer id="footer" className="footer_container grid">
+      <div className="footer_logo">
+        <a href="#" className="footer_logo">CocktailHeaven</a>
+      </div>
+
+      <div className="footer_links">
+        <ul className="footer_links_list">
+          <li className="footer_links_item">
+            <a href="#home" className="footer_links_link">Home</a>
+          </li>
+          <li className="footer_links_item">
+            <a href="#favorites" className="footer_links_link">Favourites</a>
+          </li>
+          <li className="footer_links_item">
+            <a href="#signatures" className="footer_links_link">Signature Drinks</a>
+          </li>
+          <li className="footer_links_item">
+            <a href="#shop" className="footer_links_link">Shop</a>
+          </li>
+          <li className="footer_links_item">
+            <a href="#contact-us" className="footer_links_link">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="footer_social">
+        <a href="https://www.facebook.com" className="footer_social_link">
+          <i className="ri-facebook-fill"></i>
+        </a>
+        <a href="https://www.instagram.com" className="footer_social_link">
+          <i className="ri-instagram-fill"></i>
+        </a>
+        <a href="https://www.twitter.com" className="footer_social_link">
+          <i className="ri-twitter-x-fill"></i>
+        </a>
+      </div>
+
+      <div className="footer_copy">
+        <p>&copy; 2025 Cocktail Website. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
