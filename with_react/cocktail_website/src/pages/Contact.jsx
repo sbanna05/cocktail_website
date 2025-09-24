ef30350563eaf4221307844abb194f080c6c1486
Stancz Boglár Anna <sbanna0422@gmail.com>
2025-09-24 20:32:35 +0200

    update with contact-us and shop section

--- with_react/cocktail_website/src/pages/Contact.jsx
+++ with_react/cocktail_website/src/pages/Contact.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 function Contact() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section id="contact-us" className="container contact-us_container grid">
+      <div className="contact-us_title section_title">
+        Feel Free To Contact Us
+      </div>
+
+      <div className="contact-us_info">
+        <p className="contact-us_info_title">General Contact Informations</p>
+        <p className="contact-us_info_p">Email: basic_email@email.com</p>
+        <p className="contact-us_info_p">Phone: +00 001 0012</p>
+        <p className="contact-us_info_p">
+          Address: 123 Cocktail St, Mixology City, CO 12345
+        </p>
+      </div>
+
+      <p className="form_info">
+        If you have any questions or feedback, feel free to reach out to us!
+      </p>
+
+      <form className="contact-us_form">
+        <input
+          type="text"
+          className="contact-us_input"
+          placeholder="Your Name"
+          required
+        />
+        <input
+          type="email"
+          className="contact-us_input"
+          placeholder="Your Email"
+          required
+        />
+        <input
+          type="tel"
+          className="contact-us_input"
+          placeholder="Your Phone Number"
+          required
+        />
+        <textarea
+          className="contact-us_textarea"
+          placeholder="Your Message"
+          required
+        />
+        <button type="submit" className="contact-us_button">
+          Send Message
+        </button>
+      </form>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
--- with_react/cocktail_website/src/pages/Shop.jsx
+++ with_react/cocktail_website/src/pages/Shop.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function Shop() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section id="shop" className="container shop_container grid">
+      <div className="shop_title section_title">Discover Our Latest Items</div>
+
+      <div className="shop_items">
+        <a href="shop.html" className="shop_item">
+          <img src="./src/assets/images/beverages.jpg" alt="Beverages" width={300}/>
+          <span className="shop_label">Beverages</span>
+        </a>
+
+        <a href="shop.html" className="shop_item">
+          <img src="./src/assets/images/essentials.jpg" alt="Essentials" width={300} height={250} />
+          <span className="shop_label">Essentials</span>
+        </a>
+      </div>
+    </section>
+  );
 }
 
-export default Shop
+export default Shop;
